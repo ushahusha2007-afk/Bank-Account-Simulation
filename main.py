58e488b5ca7f7e90866d18c300e32169e0a7c0bc
Usha.H <ushahusha2007@gmail.com>
2026-05-20 22:24:35 +0530

    Initial project setup

---
+++ main.py
@@ -0,0 +1,23 @@
+accounts = {}
+
+while True:
+
+    print("\n===== BANK ACCOUNT SYSTEM =====")
+    print("1. Create Account")
+    print("2. Login")
+    print("3. Exit")
+
+    choice = input("Enter your choice: ")
+
+    if choice == "1":
+        print("Create Account Selected")
+
+    elif choice == "2":
+        print("Login Selected")
+
+    elif choice == "3":
+        print("Thank You")
+        break
+
+    else:
+        print("Invalid Choice")
